aca7b8dc51da517d5e3d615f8f7b3e0e5c54cebe
Jordan22195 <Jordanroth7@gmail.com>
2022-10-04 11:48:23 -0500

    Added utc timestamp to bodystate packet in UDP Send Simulink model. Removed unused autocode files from UDP send simulink model

--- UDP_Send_Simulink_CubeSatSim/work/codegen/asbCubeSat_packngo/build.bat
+++
@@ -1,4 +0,0 @@
-arm-linux-gnueabihf-g++.exe asbCubeSat.c asbCubeSat_data.c asbCubeSatACS.c DAHostLib_Network.c  DAHostLib_rtw.c linuxinitialize.c linuxUDP.c MW_Pyserver_control.c MW_raspi_init.c rt_atan2d_snf.c rt_nonfinite.c rtGetInf.c rtGetNaN.c
-
-
-PAUSE
