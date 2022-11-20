d53af9b85e6fb6c6fd454bf5db5930558c3636a7
jruzicka <ruzickajason@gmail.com>
2022-11-19 22:02:44 -0600

    Added package installation check to orbiter launch script. Added orbiter launch command to ground station script.

--- Orbiter/Script/Launch.bat
+++ Orbiter/Script/Launch.bat
@@ -1,1 +1,11 @@
-%cd%/Package/Windows/Orbiter.exe -ResX=720 -ResY=720 -WINDOWED 
+@echo off
+if exist %~dp0/Package/Windows/Orbiter.exe (
+	echo "Launching Orbiter"
+) else (
+  echo "Orbiter has not been installed."
+  echo "Please install using the PackageOrbiter.bat"
+  pause
+  exit
+)
+
+%~dp0/Package/Windows/Orbiter.exe -ResX=720 -ResY=720 -WINDOWED 
--- cosmos-groundstation/Launcher.bat
+++ cosmos-groundstation/Launcher.bat
@@ -1,1 +1,5 @@
+set orbiterpath=%cd%/../Orbiter/Script/Launch.bat
+
+start %orbiterpath%
+
 call tools\Launcher.bat --config launcher.txt
